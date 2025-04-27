2f30956e9c74860a805bf8d633db7ee88e73bc4d
fl720 <99543089+fl720@users.noreply.github.com>
2025-04-27 17:52:12 +0100

    Delete monitor_system/page/comfitness directory

--- monitor_system/page/comfitness/comfitness.py
+++
@@ -1,9 +0,0 @@
-
-# =================== TEMPERARY FILE ====================
-def evaluate(temp: float, humidity: float) -> str:
-    if temp > 30 and humidity > 70:
-        return "Risk of heat exhaustion. Stay hydrated."
-    elif temp < 5:
-        return "Very cold. Dress warmly and avoid exposure."
-    else:
-        return "Conditions are normal. Stay active and healthy!"
